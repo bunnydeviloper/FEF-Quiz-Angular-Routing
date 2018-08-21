ea976692d6e5918e08b148bb8adcfda3b7fd0618
Sophia Mai <phuongmai001@gmail.com>
2018-08-20 19:24:25 -0700

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,35 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/'); // make sure any unknown state will be redirected to root
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/instructions.html',
+        // has no controller
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      })
+      .state('brick', {
+        url: '/bricks/:color',
+        templateUrl: 'views/bricks.html',
+        controllerProvider: function($stateParams) {
+          var color = $stateParams.color;
+          color = color[0].toUpperCase() + color.slice(1);
+          var ctrlName = color + 'BricksCtrl';
+          return ctrlName;
+        },
+        controllerAs: 'brick'
+      })
+      .state('brick.cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      });
+  }]);
